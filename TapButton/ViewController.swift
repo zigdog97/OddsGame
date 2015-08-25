d8a70d2f8642b532a83e44db3c1afa8e7f64c6dc
Atal Patel <atalpatel@Atals-MacBook-Pro.local>
2015-08-25 19:03:34 -0400

    Initial Commit

---
+++ TapButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapButton
+//
+//  Created by Atal Patel on 8/25/15.
+//  Copyright (c) 2015 Atal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
